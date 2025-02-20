c1b66ced030e84f86b02a231bf89fc5e34c58833
bishwajit <arin17bishwa@gmail.com>
2025-02-21 00:50:04 +0530

    PN5

--- app/main.py
+++ app/main.py
@@ -5,6 +5,8 @@ def main():
     while True:
         sys.stdout.write("$ ")
         command: str = input()
+        if command == 'exit 0':
+            sys.exit(0)
         print(f"{command}: command not found")
 
 
