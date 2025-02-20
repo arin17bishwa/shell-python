0cb1f5403bec9cff8925521d518e5fb879430d8e
bishwajit <arin17bishwa@gmail.com>
2025-02-21 01:02:01 +0530

    PN5 modification

--- app/main.py
+++ app/main.py
@@ -2,12 +2,34 @@ import sys
 
 
 def main():
+    command_mapping: dict = {
+        'echo': _echo,
+        'exit': _exit,
+    }
+
     while True:
         sys.stdout.write("$ ")
-        command: str = input()
-        if command == 'exit 0':
-            sys.exit(0)
-        print(f"{command}: command not found")
+        command_str: str = input()
+        if not command_str:
+            pass
+        command_args = command_str.split()
+        command = command_args[0]
+        command_func = command_mapping.get(command)
+
+        if not command_func:
+            print(f"{command}: command not found")
+        else:
+            command_func()
+
+
+def _exit(*args):
+    exit_status = int(args[1])
+    print(args)
+    sys.exit(0)
+
+
+def _echo(*args):
+    print(' '.join(args[1:]))
 
 
 if __name__ == "__main__":
